b1929766ee5210bcd79ab194094a85d965e57652
nicolas <nicolas.jan.stas@gmail.com>
2022-12-07 20:00:12 +0100

    added post

--- frontend.py
+++ frontend.py
@@ -18,5 +18,36 @@ try:
 except:
     st.write("error connecting to the server")
 
-st.write("contribute to the project on github: ")
+st.write("## add your name to the list !!")
+#  add a text input to the page
+#  when the user presses enter, send a post request to the server
+#  with the text input as the body of the request
+#  the server should add the text to the list of death wishes
+#  and return "OK"
+#  if the server returns "OK", display a message saying "your name has been added to the list"
+#  if the server returns anything else, display a message saying "there was an error adding your name to the list"
+
+#  add a text input to the page
+st.text_input("enter your name here", key="deathWish")
+
+st.button("submit", key="submit")
+
+#  when the user presses enter, send a post request to the server
+#  with the text input as the body of the request
+text = st.session_state.deathWish
+if st.session_state.submit:
+    server_dest = f"http://{BACKEND_IP}:{BACKEND_PORT}/wishes"
+    try:
+        response = requests.post(server_dest, data=text)
+        if response.text == "OK":
+            st.write("your name has been added to the list")
+        else:
+            st.write("there was an error adding your name to the list")
+    except:
+        st.write("there was an error adding your name to the list")
+
+
+
+#  add to the bottom of the page a link to the github repo
+st.write("## contribute to the project")
 st.write("https://github.com/COLVERTYETY/DVIC_deathWish")
